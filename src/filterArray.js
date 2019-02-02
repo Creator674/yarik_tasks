a4efd9a9eb70f481e0bacf4745b5468565031414
Creator674 <666anastyalena@mail.ru>
2019-02-02 22:48:54 +0300

    completed

--- src/filterArray.js
+++ src/filterArray.js
@@ -1,4 +1,10 @@
 module.exports = function filterArray(array) {
   // твое решение здесь
-  return array
+  var arr1 = [];
+  for ( var i = 0; i<array.length; i++){
+    if ( array[i] % 2 == 1){
+      arr1.push(array[i]);
+  }
+}
+  return arr1
 }
